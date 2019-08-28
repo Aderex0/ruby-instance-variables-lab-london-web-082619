015b03ab484d2cd1e1f877218138a00563413338
Andrius Jurgsaitis <andriusj6@hotmail.com>
2019-08-28 13:50:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,10 @@
 class Dog
   def initalize(dog_name, age)
     @this_dogs_name = dog_name
-    @dogs_age = age
   end
  
   def name
     @this_dogs_name
-  end
-  
-  def age
-    @dogs_age
   end
 end
 
